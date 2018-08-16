b61b3c34ff55182ce7e1c9a82bba1638108441c3
Ivan Besarab <Ivan.Besarab@RockSoft.rocks>
2018-08-16 20:49:22 +0300

    Initial Commit

---
+++ MyRobot/SwiftRobotControlCenter.swift
@@ -0,0 +1,92 @@
+//
+//  SwiftRobotControlCenter.swift
+//  MyRobot
+//
+//  Created by Ivan Vasilevich on 10/4/14.
+//  Copyright (c) 2014 Ivan Besarab. All rights reserved.
+//
+
+import UIKit
+//all robot commands can be founded in GameViewController.h
+class SwiftRobotControlCenter: RobotControlCenter {
+	//	dfkjldjs;fds;fas;
+	//	klfd;sk;fks;
+	func l0c() {
+		move()
+		if frontIsClear {
+			doubleMove()
+			pick()
+			doubleMove()
+			turnRight()
+			move()
+			put()
+			turnLeft()
+			doubleMove()
+		}
+		else {
+			put()
+		}
+		turnRight()
+		turnLeft()
+	}
+	
+	func forLoopExample() {
+		for _ in 0..<14 {
+			put()
+			move()
+		}
+		put()
+	}
+	
+	//in this function change levelName
+	override func viewDidLoad() {
+		levelName = "L3C" // level name
+
+		super.viewDidLoad()
+	}
+	
+	override func viewDidAppear(_ animated: Bool) {
+		/*
+		fl;dkf
+		;dskfdskfd;kfsdkf
+		dskf
+		dsfkdsfksd
+		fkas
+		
+		*/
+		super.viewDidAppear(animated)
+		
+		// loop to search candy
+		while noCandyPresent { // loop to search candy
+			if frontIsClear {
+				move()
+			}
+			else {
+				break
+			}
+			put()
+			pick()
+		}
+		turnRight()
+		turnLeft()
+		
+		
+		
+		
+		
+	}
+	
+	func turnLeft() {
+		for _ in 0..<3 {
+			turnRight()
+		}
+	}
+	
+	func doubleMove() {
+		move()
+		move()
+	}
+	
+	
+	
+}
